c2098f5c5ac397afefca67e98903f1b5c1062c1a
Eva SV <evasauraklix@hotmail.com>
2026-04-22 12:48:41 +0200

    Ejercicio 2 condicionales

---
+++ ejercicios/t4e2guessthenumber.py
@@ -0,0 +1,23 @@
+def guessNumber(number):
+    if (number == 4):
+        message = "You have won"
+    
+    elif (number == 1,2,3,5,6,7,8,9,10):
+        message = "You have lost"
+    return message
+
+
+
+# Ask the user for a number between 1 and 10
+
+number = float (input("Enter a number between 1 and 10: "))
+
+# We call the guess number to give us the message
+
+message = guessNumber(number)
+
+# We print the message with the choosen number
+
+print(message)
+
+
